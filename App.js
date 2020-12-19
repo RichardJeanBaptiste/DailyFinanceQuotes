7b089d185cdf4d7011adc51fc4f123d766713b03
Richard Jean-Baptiste <Richinbk1@gmail.com>
2020-12-18 19:39:17 -0500

    added react-navigation

--- App.js
+++ App.js
@@ -12,9 +12,21 @@
 import React, {Component} from 'react';
 import {SafeAreaView, View, Text, StyleSheet, Button} from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Quotes from './components/Quotes';
+
+const MyTheme = {
+  dark: false,
+  colors: {
+    //primary: 'rgb(255, 45, 85)',
+    background: 'rgb(31,25,18)',
+    //card: 'rgb(255, 255, 255)',
+    text: 'white',
+    //border: 'rgb(199, 199, 204)',
+    //notification: 'rgb(255, 69, 58)',
+  },
+};
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -22,13 +34,13 @@ const Drawer = createDrawerNavigator();
 const HomeStack = createStackNavigator();
 const HomeStackScreen = ({ navigation }) => (
   <Stack.Navigator>
-        <Stack.Screen name="Home" component={Quotes} options={{title: 'Daily Finance Quote'}}/>
+        <Stack.Screen name="Home" component={Quotes} options={{title: 'Daily Finance Quote', headerTitleAlign: 'center'}}/>
   </Stack.Navigator>
 );
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={MyTheme}>
       <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen name="Home" component={HomeStackScreen} />
       </Drawer.Navigator>
