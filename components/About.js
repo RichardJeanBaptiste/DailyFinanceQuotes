473f9b18690ebc389777eeb9b2c5169528aa31e3
Richard Jean-Baptiste <Richinbk1@gmail.com>
2022-08-21 23:50:55 -0400

    added functionality to about page - added app review - added change log modal - added links to email

--- components/About.js
+++ components/About.js
@@ -1,14 +1,19 @@
 /* eslint-disable prettier/prettier */
 
 
-import React, { } from 'react';
-import {SafeAreaView, Text, View, Image, ScrollView} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {SafeAreaView, Text, View, Image, ScrollView, Pressable, Linking, Modal, Dimensions, Button} from 'react-native';
 import ProfPic from '../assets/prof.jpg';
 import Divider from './Divider';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+//import InAppReview from 'react-native-in-app-review';
+import Rate, { AndroidMarket } from 'react-native-rate';
 
 
 function About(){
+
+    const windowWidth = Dimensions.get('window').width;
+    const windowHeight = Dimensions.get('window').height;
 
     const Styles = {
         profileView: {
@@ -86,12 +91,114 @@ function About(){
             color: 'white',
             fontSize: 18,
         },
+        centeredView: {
+            //flex: 1,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: 20,
+            width: windowWidth - 65,
+            height: windowHeight - 60,
+            backgroundColor: '#2a2f3c',
+            borderRadius: 25,
+        },
+        modalFooterView: {
+            display: 'flex',
+            flexDirection: 'row',
+            width: windowWidth - 80,
+            position: 'absolute',
+            bottom: 20,
+        },
+        modalFooterText: {
+            color: 'orange',
+            fontSize: 15,
+            marginRight: 'auto',
+            marginLeft: '3%',
+        },
+        modalFooterText2: {
+            color: 'orange',
+            fontSize: 15,
+            marginLeft: 'auto',
+            marginRight: '3%',
+        },
     };
 
+    const [modalVisible, setModalVisible] = useState(false);
+    //const [ canReviewApp, setCanReviewApp ] = useState(false);
+
+    /*
+    useEffect(() => {
+        console.log(InAppReview.isAvailable());
+    },[]);
+    */
+
+
+    const reportBugs = () => {
+        Linking.openURL('mailto:Richinbk1@gmail.com');
+    };
+
+    const linkToMe = () => {
+        Linking.openURL('mailto:Richinbk1@gmail.com');
+    };
+
+    const RateButton = () => {
+        const [rated, setRated] = useState(false);
+        return (
+            <View>
+                <Button title="Rate App" onPress={()=>{
+                const options = {
+                    //AppleAppID:"2193813192",
+                    GooglePackageName:'com.financequotes',
+                    //AmazonPackageName:"com.mywebsite.myapp",
+                    //OtherAndroidURL:"http://www.randomappstore.com/app/47172391",
+                    preferredAndroidMarket: AndroidMarket.Google,
+                    preferInApp:false,
+                    openAppStoreIfInAppFails:true,
+                    //fallbackPlatformURL:"http://www.mywebsite.com/myapp.html",
+                };
+                Rate.rate(options, (success, errorMessage)=>{
+                    if (success) {
+                    // this technically only tells us if the user successfully went to the Review Page. Whether they actually did anything, we do not know.
+                        setRated(true);
+                    }
+                    if (errorMessage) {
+                    // errorMessage comes from the native code. Useful for debugging, but probably not for users to view
+                        console.error(`Example page Rate.rate() error: ${errorMessage}`);
+                    }
+                });
+                }} />
+            </View>
+        );
+    };
+
+    const ChangeLogModal = () => {
+
+        return (
+            <View>
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                    setModalVisible(false);
+                    }}
+                >
+                    <View style={Styles.centeredView}>
+                        <View style={Styles.modalFooterView}>
+                            <Text style={Styles.modalFooterText}>Rate App</Text>
+                            <Text style={Styles.modalFooterText2} onPress={() => setModalVisible(false)}>Close</Text>
+                        </View>
+                    </View>
+                </Modal>
+            </View>
+        );
+    };
 
     return (
         <SafeAreaView>
             <ScrollView>
+                <ChangeLogModal/>
                 <View style={Styles.profileView}>
                     <Image
                         style={Styles.profilePic}
@@ -107,26 +214,38 @@ function About(){
                 <View style={Styles.appView}>
                     <Text style={Styles.appText}>Daily Finance Inspiration</Text>
 
-                    <View style={Styles.rateAppView}>
-                        <Text style={Styles.rateAppText}>Rate App</Text>
-                        <Text style={Styles.rateAppText2}>Help me out by rating the app on the Google Play Store</Text>
-                    </View>
+                    <RateButton/>
+                    <Pressable>
+                        <View style={Styles.rateAppView}>
+                            <Text style={Styles.rateAppText}>Rate App</Text>
+                            <Text style={Styles.rateAppText2}>Help me out by rating the app on the Google Play Store</Text>
+                        </View>
+                    </Pressable>
 
-                    <View style={Styles.rateAppView}>
-                        <Text style={Styles.rateAppText}>Report Bug</Text>
-                        <Text style={Styles.rateAppText2}>Report bugs or request new features</Text>
-                    </View>
+                    <Pressable onPress={reportBugs}>
+                        <View style={Styles.rateAppView}>
+                            <Text style={Styles.rateAppText}>Report Bug</Text>
+                            <Text style={Styles.rateAppText2}>Report bugs or request new features</Text>
+                        </View>
+                    </Pressable>
 
-                    <View style={Styles.rateAppView}>
-                        <Text style={Styles.rateAppText}>Change Logs</Text>
-                        <Text style={Styles.rateAppText2}>Updates to the apps</Text>
-                    </View>
+                    <Pressable onPress={() => setModalVisible(!modalVisible)}>
+                        <View style={Styles.rateAppView}>
+                            <Text style={Styles.rateAppText}>Change Logs</Text>
+                            <Text style={Styles.rateAppText2}>Updates to the apps</Text>
+                        </View>
+                    </Pressable>
+
                 </View>
                 <Divider/>
-                <View style={Styles.developerView}>
-                    <Text style={Styles.developerText}>Developer</Text>
-                    <Text style={Styles.developerText2}> <FontAwesome5 name="envelope" style={Styles.emailIcon}/>     Richinbk1@gmail.com</Text>
-                </View>
+
+                <Pressable onPress={linkToMe}>
+                    <View style={Styles.developerView}>
+                        <Text style={Styles.developerText}>Developer</Text>
+                        <Text style={Styles.developerText2}> <FontAwesome5 name="envelope" style={Styles.emailIcon}/>     Richinbk1@gmail.com</Text>
+                    </View>
+                </Pressable>
+
                 <Divider/>
                 <View style={Styles.privacyPolicyView}>
                     <Text style={Styles.genericText}>Privacy Policy</Text>
